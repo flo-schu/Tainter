9ffc03998532f2306ab5320d63adba908df7d643
flo-schu <fluncki@protonmail.com>
2019-04-24 18:35:05 +0200

    remove seed

--- scripts/tests/model_test.py
+++ scripts/tests/model_test.py
@@ -30,8 +30,8 @@ else:
 #foldername = input("Foldername")
 again = "y"
 while again == "y":
-    random.seed(2)
-    np.random.seed(2)
+    random.seed(4)
+    np.random.seed(4)
     # expl = float(input("set exploration [0,1]"))
     expl = specs[iter][0]
     N = 400
@@ -136,46 +136,3 @@ with open("../../results/model/"+folder+"/network.pickle","wb") as handle:
 
 print("saved all data!")
 # input("plot?")
-
-
-fig, (ax1,ax3) = plt.subplots(2,1,sharex=True)
-# norm = mpl.colors.Normalize(vmin=0, vmax=6)
-cmap = cm.get_cmap("tab20",20)
-# cmap = cm.get_cmap("Dark2",8)
-ax3.plot('x','Admin',ls='-',fillstyle='none', data = data_explore,
-    label = "$A_{total}$", color = cmap(0))
-ax3.fill_between(data_explore['x'],data_explore['A_pool_shk'],
-    label = "$A_{shock}$",alpha = .5,color = cmap(0))
-ax3.fill_between(data_explore['x'],data_explore['A_pool_exp']+data_explore['A_pool_shk'],
-    data_explore['A_pool_shk'],
-    label = "$A_{exploration}$",alpha = .5,color = cmap(1))
-ax4 = ax3.twinx()
-ax4.plot(data_explore['x'], data_explore['Ecap'],
-    label = "$E_{prodcution}$", c = "g", ls = "-", lw = .5,color = cmap(2))
-
-ax3.set_ylim(0,1)
-ax2= ax1.twinx()
-ax1.plot(data_base['x'],data_base['Admin'],ls='-',fillstyle='none',color = cmap(0))
-
-ax1.fill_between(data_base['x'],data_base['A_pool_shk'],
-    alpha = .5,color = cmap(0))
-ax1.fill_between(data_base['x'],data_base['A_pool_exp']+data_base['A_pool_shk'],
-    data_base['A_pool_shk'],
-    alpha = .5,color = cmap(1))
-ax2.plot(data_base['x'], data_base['Ecap'], c = "g", ls = "-", lw = .5,color = cmap(2))
-
-ax1.set_ylim(0,1)
-plt.setp(ax1.get_xticklabels(), visible=False)
-ax4.set_ylim(0,max(np.append(data_base["Ecap"],data_explore['Ecap']))+.1)
-ax2.set_ylim(0,max(np.append(data_base["Ecap"],data_explore['Ecap']))+.1)
-fig.legend(loc = "upper right", bbox_to_anchor = (.9,.88),ncol = 1)
-fig.text(0.05, 0.5, 'Administrator share', ha='center',
-    va='center', rotation='vertical')
-fig.text(0.98, 0.5, 'Energy per capita', ha='center',
-    va='center', rotation='vertical')
-fig.text(0.5, 0.04, 'Time', ha='center', va='center')
-
-ax1.annotate("A", xy=(0.01, 0.9), xycoords="axes fraction")
-ax3.annotate("B", xy=(0.01, 0.9), xycoords="axes fraction")
-plt.show()
-fig.savefig("../../results/model/"+folder+"/Admin_Ecap_twocases.png")
