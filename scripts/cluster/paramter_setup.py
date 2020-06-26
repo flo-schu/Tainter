dde6d3d2186d26ee0674725e52448982b2f20542
flo-schu <fluncki@protonmail.com>
2020-06-26 15:32:02 +0200

    fit paramter setup file to tainter

--- scripts/cluster/paramter_setup.py
+++ scripts/cluster/paramter_setup.py
@@ -1,14 +1,17 @@
-# folder = "20190419_1134"
-folder = "20200625_16302"
-  # os.makedirs("../../results/model/"+folder)
-rho     = np.linspace(0,0.3,51 )  # link density in erdos renyi network
-phi     = np.linspace(1,1.5,51)   # efficiency of coordinated Workers
-# pe_range= np.logspace(np.log10(0.0001),np.log10(.02),21)
-pe_explore = np.logspace(-4,-1.6,num = 51)
+import csv
+import numpy as np
+import itertools as it
+
+rho = np.linspace(0, 0.3, 11)  # link density in erdos renyi network
+phi = np.linspace(1, 1.5, 11)  # efficiency of coordinated Workers
+
 pe_null = np.array([0])
+pe_explore = np.logspace(-4, -1.6, num=11)
 
 pe_range = np.concatenate((pe_null, pe_explore), axis=None)
-pargrid = it.product(rho, phi)
+pargrid = it.product(pe_range, rho, phi)
 
-print(pe_range)
-input("press enter")
+with open("params.csv", "w", newline="") as file:
+    f = csv.writer(file)
+    f.writerow(["pe", "rho", "phi"])
+    f.writerows(pargrid)
