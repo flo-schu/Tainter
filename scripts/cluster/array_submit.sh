9f2d239892cd084efc4d6e54f6681df5ea829ec3
flo-schu <fluncki@protonmail.com>
2020-06-26 18:47:51 +0200

    array_submit.sh is now working on the cluster superbly

--- scripts/cluster/array_submit.sh
+++ scripts/cluster/array_submit.sh
@@ -11,11 +11,11 @@
 output_dir="/work/$USER/$JOB_NAME/$JOB_ID"
 mkdir -p "$output_dir"
 
-module load Anaconda3
+module load Anaconda3/5.3.0
 source activate .conda/envs/tainter
 
 echo "processing chunk $SGE_TASK_ID ..."
 
-python ./tainter/paramter_analysis_20200626/paramter_scan.py "./tainter/paramter_analysis_20200626/params/chunk_$SGE_TASK_ID.txt" "$output_dir" "$SGE_TASK_ID"
+python ./tainter/paramter_analysis_20200626/parameter_scan.py "./tainter/paramter_analysis_20200626/params/chunk_$SGE_TASK_ID.txt" "$output_dir" "$SGE_TASK_ID"
 
 source deactivate
