9dba0a26d51fdab6d07daa91b3d301683269239f
flo-schu <fluncki@protonmail.com>
2020-06-27 12:27:53 +0200

    increase maximum runtime for scripts; also add some checks for files
    add some checks to the submit file to see whether files exist.

--- cluster/parameter_analysis_20200626/array_submit.sh
+++ cluster/parameter_analysis_20200626/array_submit.sh
@@ -3,9 +3,9 @@
 #$ -S /bin/bash
 #$ -wd /home/schunckf
 #$ -N tainter
-#$ -l h_rt=00:05:00
+#$ -l h_rt=00:30:00
 #$ -l h_vmem=1G
-#$ -o /work/$USER/$JOB_NAME/$JOB_ID/log.txt
+#$ -o /work/$USER/$JOB_NAME/$JOB_ID/log_$TASK_ID.txt
 #$ -j y
 #$ -binding linear:1
 output_dir="/work/$USER/$JOB_NAME/$JOB_ID"
@@ -16,6 +16,18 @@ source activate .conda/envs/tainter
 
 echo "processing chunk $SGE_TASK_ID ..."
 
-python ./tainter/paramter_analysis_20200626/parameter_scan.py "./tainter/paramter_analysis_20200626/params/chunk_$SGE_TASK_ID.txt" "$output_dir" "$SGE_TASK_ID"
+PARFILE=./tainter/parameter_analysis_20200626/params/chunk_$SGE_TASK_ID.txt
+SCRIPTFILE=./tainter/parameter_analysis_20200626/parameter_scan.py
+if test -f "$PARFILE"; then
+    echo "$PARFILE exists."
+fi
+if test -f "$SCRIPTFILE"; then
+    echo "$SCRIPTFILE exists."
+fi
 
+echo "..."
+
+python ./tainter/parameter_analysis_20200626/parameter_scan.py "./tainter/parameter_analysis_20200626/params/chunk_$SGE_TASK_ID.txt" "$output_dir" "$SGE_TASK_ID"
+
+echo "chunk successfully executed"
 source deactivate
