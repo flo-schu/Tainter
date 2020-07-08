8490623e96b269bbc0f8247db061617af8b408eb
flo-schu <fluncki@protonmail.com>
2020-07-08 18:11:01 +0200

    print numbers of missing output chunks

--- cluster/parameter_analysis_20200630/output_processing.py
+++ cluster/parameter_analysis_20200630/output_processing.py
@@ -10,13 +10,25 @@
 # ------------------------------------------------------------------------------
 
 import numpy as np
+import re
 import os
 
 # in this script I use type hints to declare the type of my variables used
 # for type checking for better debugging and later code analysis.
 
 params_files = os.listdir("params/")
-output_files = os.listdir("output2/")  # returns list of files
+output_files = os.listdir("output/")  # returns list of files
+
+par = sorted([int(re.findall('[0-9]+', i)[0]) for i in params_files])
+out = sorted([int(re.findall('[0-9]+', i)[0]) for i in output_files])
+
+print(par)
+print(out)
+
+print(len(par), len(out))
+missing = [p for p in par if p not in out]
+
+print(missing)
 
 if len(params_files) == len(output_files):
     pass
@@ -26,7 +38,7 @@ else:
 output = np.ndarray(shape=(0, 5))
 for i in range(len(output_files)):
     print(output_files[i])
-    chunk = np.loadtxt("./output2/"+output_files[i], delimiter=",", ndmin=2)
+    chunk = np.loadtxt("./output/"+output_files[i], delimiter=",", ndmin=2)
     output = np.concatenate((output, chunk), axis=0)
 
 colnames = "p_e, rho, phi, te, st"
