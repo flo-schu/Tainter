100b0f1cced3d60232e902d14b75b1f44a2198bc
flo-schu <fluncki@protonmail.com>
2020-07-08 14:18:55 +0200

    no so good working version of odeint

--- cluster/parameter_analysis_20200630/parameter_scan_odeint.py
+++ cluster/parameter_analysis_20200630/parameter_scan_odeint.py
@@ -14,15 +14,16 @@
 import sys
 import numpy as np
 import scipy.stats as sci
-from scipy.integrate import ode
+from scipy.integrate import odeint
 from scipy.integrate import trapz
+import matplotlib.pyplot as plt
 
 # environmental variables -----------------------------------------------------
 paramfile = sys.argv[1]
 output_dir = sys.argv[2]
 njob = int(sys.argv[3])
 
-print(paramfile, output_dir, njob)
+# print(paramfile, output_dir, njob)
 
 # Parameters ------------------------------------------------------------------
 N = 400  # Network size
@@ -32,25 +33,17 @@ alpha = 1  # location parameter of beta distribution
 # p_e, rho, phi
 params = np.loadtxt(paramfile, delimiter=",")
 
-print(params)
+
+# print(params)
+
+
 # Definition Equations --------------------------------------------------------
 def f_a(t, x, N, p_e, epsilon, rho, phi, beta, alpha):
     return (
-            p_e * (N - 2 * x) +
-            sci.beta.cdf(
-                (((epsilon * N) / (((N - x) * (1 - rho) ** x) +
-                                   ((N - x) * (1 - (1 - rho) ** x)) ** phi))),
-                a=beta, b=alpha)
-    )
-
-
-def f_s(x, N, p_e, epsilon, rho, phi, beta, alpha):
-    return (
-            p_e * (N - 2 * x) +
-            sci.beta.cdf(
-                (((epsilon * N) / (((N - x) * (1 - rho) ** x) +
-                                   ((N - x) * (1 - (1 - rho) ** x)) ** phi))),
-                a=beta, b=alpha)
+        p_e * (N - 2 * x) + sci.beta.cdf(
+        (((epsilon * N) / (((N - x) * (1 - rho) ** x) +
+        ((N - x) * (1 - (1 - rho) ** x)) ** phi))),
+        a=beta, b=alpha)
     )
 
 
@@ -60,38 +53,27 @@ def f_e(x, N, rho, phi):
     )
 
 
-# Solver ----------------------------------------------------------------------
-def get_timeseries(timestep, tmax, initial_value, p_e, rho, phi):
-    r = ode(f_a)
-    r.set_initial_value(initial_value)
-    r.set_f_params(N, p_e, epsilon, rho, phi, beta, alpha)
-    r.set_integrator("vode")
-
-    t = [0]
-    results = np.array([initial_value])
-    while r.t < tmax and r.successful():
-        print(r.t)
-
-        r.integrate(r.t + timestep)
-        t.append(r.t)
-        results = np.append(results, r.y)
-
-    return t, results
+def get_st(t, e):
+    return t[e <= 0]
 
 
+t = np.linspace(0, 10000, 10000)
 data = []
 
 for i in range(len(params)):
     print(i)
     par = params[i]
-    print(par)
     p_e, rho, phi = par[0], par[1], par[2]
-    print("...")
-    t, x = get_timeseries(1, 10000, 0, p_e, rho, phi)
-    print("...")
-    e = f_e(np.array(x), N, rho, phi)
+    x = odeint(f_a, y0=0, t=t, args=(N, p_e, epsilon, rho, phi, beta, alpha))
+    e = f_e(np.array(x[:, 0]), N, rho, phi)
+    print(e)
+    plt.plot(t, x / N, label="admin")
+    plt.plot(t, e, label="energy")
+    plt.legend()
+    plt.show()
     te = trapz(e, t)
-    st = t[-1]
+    st = get_st(t, e)[0]
+
     data.append(np.array([p_e, rho, phi, te, st]))
     print("pe, rho, phi: ", par, " -- st: ", st, " -- te: ", te)
 
