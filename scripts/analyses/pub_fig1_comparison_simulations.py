449df6f66cf11c764ff551f1950806f924cdccd9
flo-schu <fluncki@protonmail.com>
2020-07-08 18:12:36 +0200

    update figures 1 and 2
    fig 1: change axis limits
    fig 2: change density curves to histograms

--- scripts/analyses/pub_fig1_comparison_simulations.py
+++ scripts/analyses/pub_fig1_comparison_simulations.py
@@ -11,84 +11,82 @@ import numpy as np
 # folder = "20191121_1448"
 folder = "20191121_1734"
 
-data_explore = pd.read_csv("../../results/model/"+folder+"/t5_explore.csv")
-data_base    = pd.read_csv("../../results/model/"+folder+"/t5_base.csv")
-data_interme = pd.read_csv("../../results/model/"+folder+"/t5_intermediate.csv")
+data_explore = pd.read_csv("../../results/model/" + folder + "/t5_explore.csv")
+data_base = pd.read_csv("../../results/model/" + folder + "/t5_base.csv")
+data_interme = pd.read_csv("../../results/model/" + folder + "/t5_intermediate.csv")
 
 textwidth = 12.12537
 plt.rcParams.update({'font.size': 14})
-fig, (ax1,ax3,ax5) = plt.subplots(3,1,sharex=True,
-                                  figsize = (textwidth,textwidth/2))
+fig, (ax1, ax3, ax5) = plt.subplots(3, 1, sharex=True,
+                                    figsize=(textwidth, textwidth / 2))
 # norm = mpl.colors.Normalize(vmin=0, vmax=6)
-cmap = cm.get_cmap("tab20",20)
+cmap = cm.get_cmap("tab20", 20)
 # cmap = cm.get_cmap("Dark2",8)
-ax2= ax1.twinx()
+ax2 = ax1.twinx()
 ax1.plot(np.array(data_base['x']),
          np.array(data_base['Admin']),
-         ls='-',fillstyle='none',color = cmap(0))
+         ls='-', fillstyle='none', color=cmap(0))
 
-ax1.fill_between(np.array(data_base['x']),np.array(data_base['A_pool_shk']),
-                 alpha = .5,color = cmap(0))
+ax1.fill_between(np.array(data_base['x']), np.array(data_base['A_pool_shk']),
+                 alpha=.5, color=cmap(0))
 ax1.fill_between(np.array(data_base['x']),
-                 np.array(data_base['A_pool_exp'])+np.array(data_base['A_pool_shk']),
+                 np.array(data_base['A_pool_exp']) + np.array(data_base['A_pool_shk']),
                  np.array(data_base['A_pool_shk']),
-                 alpha = .5,color = cmap(1))
-ax1.set_ylim(0,1)
-ax2.set_ylim(0,1.25)
+                 alpha=.5, color=cmap(1))
+ax1.set_ylim(0, 1.25)
+ax2.set_ylim(0, 1.25)
 ax2.plot(np.array(data_base['x']),
          np.array(data_base['Ecap']),
-         ls = "-", lw = .5,color = cmap(2))
+         ls="-", lw=.5, color=cmap(2))
 
 ax4 = ax3.twinx()
-ax3.plot(data_interme['x'],data_interme['Admin'],
-         ls='-',fillstyle='none',
-         color = cmap(0))
-ax3.fill_between(data_interme['x'],data_interme['A_pool_shk'],
-                 alpha = .5,color = cmap(0))
+ax3.plot(data_interme['x'], data_interme['Admin'],
+         ls='-', fillstyle='none',
+         color=cmap(0))
+ax3.fill_between(data_interme['x'], data_interme['A_pool_shk'],
+                 alpha=.5, color=cmap(0))
 ax3.fill_between(data_interme['x'],
-                 data_interme['A_pool_exp']+data_interme['A_pool_shk'],
+                 data_interme['A_pool_exp'] + data_interme['A_pool_shk'],
                  data_interme['A_pool_shk'],
-                 alpha = .5,color = cmap(1))
-ax3.set_ylim(0,1)
-ax4.set_ylim(0,1.25)
+                 alpha=.5, color=cmap(1))
+ax3.set_ylim(0, 1.25)
+ax4.set_ylim(0, 1.25)
 ax4.plot(data_interme['x'], data_interme['Ecap'],
-         ls = "-", lw = .5,color = cmap(2))
+         ls="-", lw=.5, color=cmap(2))
 
 ax6 = ax5.twinx()
-ax5.plot('x','Admin',ls='-',fillstyle='none',
-         data = data_explore,
-         label = "$A_{total}$",
-         color = cmap(0))
-ax5.fill_between(data_explore['x'],data_explore['A_pool_shk'],
-    label = "$A_{shock}$",alpha = .5,color = cmap(0))
-ax5.fill_between(data_explore['x'],data_explore['A_pool_exp']+data_explore['A_pool_shk'],
-    data_explore['A_pool_shk'],
-    label = "$A_{exploration}$",alpha = .5,color = cmap(1))
-ax5.set_ylim(0,1)
-ax6.set_ylim(0,1.25)
+ax5.plot('x', 'Admin', ls='-', fillstyle='none',
+         data=data_explore,
+         label="$A_{total}$",
+         color=cmap(0))
+ax5.fill_between(data_explore['x'], data_explore['A_pool_shk'],
+                 label="$A_{shock}$", alpha=.5, color=cmap(0))
+ax5.fill_between(data_explore['x'], data_explore['A_pool_exp'] + data_explore['A_pool_shk'],
+                 data_explore['A_pool_shk'],
+                 label="$A_{exploration}$", alpha=.5, color=cmap(1))
+ax5.set_ylim(0, 1.25)
+ax6.set_ylim(0, 1.25)
 ax6.plot(data_explore['x'], data_explore['Ecap'],
-    label = "$E_{prodcution}$", ls = "-", lw = .5,color = cmap(2))
-
-
+         label="$E_{prodcution}$", ls="-", lw=.5, color=cmap(2))
 
 plt.setp(ax1.get_xticklabels(), visible=False)
-ax4.set_ylim(0,max(np.append(data_base["Ecap"],data_explore['Ecap']))+.1)
-ax2.set_ylim(0,max(np.append(data_base["Ecap"],data_explore['Ecap']))+.1)
+ax4.set_ylim(0, max(np.append(data_base["Ecap"], data_explore['Ecap'])) + .1)
+ax2.set_ylim(0, max(np.append(data_base["Ecap"], data_explore['Ecap'])) + .1)
 fig.text(0.01, 0.5, 'Administrator share', ha='center',
-    va='center', rotation='vertical')
+         va='center', rotation='vertical')
 fig.text(0.99, 0.5, 'Energy per capita', ha='center',
-    va='center', rotation='vertical')
+         va='center', rotation='vertical')
 fig.text(0.5, 0.01, 'Time', ha='center', va='center')
-fig.subplots_adjust(bottom=0.08, left = 0.06, right = 0.94,top = .95)
-fig.legend(loc = "upper center", bbox_to_anchor = (.5,.95   ),ncol = 4,
-           frameon = False)
+fig.subplots_adjust(bottom=0.08, left=0.06, right=0.94, top=.95)
+fig.legend(loc="upper center", bbox_to_anchor=(.5, .95), ncol=4,
+           frameon=False)
 
 ax1.annotate("A", xy=(0.01, 0.83), xycoords="axes fraction")
 ax3.annotate("B", xy=(0.01, 0.83), xycoords="axes fraction")
 ax5.annotate("C", xy=(0.01, 0.83), xycoords="axes fraction")
-ax1.annotate("$p_{e}$ = 0",     xy=(0.85, 0.83), xycoords="axes fraction")
+ax1.annotate("$p_{e}$ = 0", xy=(0.85, 0.83), xycoords="axes fraction")
 ax3.annotate("$p_{e}$ = 0.00275", xy=(0.85, 0.83), xycoords="axes fraction")
-ax5.annotate("$p_{e}$ = 0.02",  xy=(0.85, 0.83), xycoords="axes fraction")
+ax5.annotate("$p_{e}$ = 0.02", xy=(0.85, 0.83), xycoords="axes fraction")
 # fig.tight_layout()
 plt.show()
-fig.savefig("../../results/model/"+folder+"/pub_figure2.png")
+fig.savefig("../../results/model/" + folder + "/pub_figure2.png")
--- scripts/analyses/pub_fig2c_analytic_approximation.py
+++ scripts/analyses/pub_fig2c_analytic_approximation.py
@@ -15,7 +15,7 @@ import seaborn as sns
 
 
 # Parameters ###################################################################
-N   = 400       # Network size
+N = 400  # Network size
 
 # if case == "explore":
 #     p_e = 0.02      # exploration probability
@@ -24,30 +24,30 @@ N   = 400       # Network size
 # else:
 #     case = input("plot explore or base?")
 
-epsilon = 1     # threshold
-rho     = 0.02  # link density in erdos renyi network
-phi     = 1.05  # efficiency of coordinated Workers
-beta    = 15    # scale parameter of beta distribution
-alpha   = 1     # location parameter of beta distribution
+epsilon = 1  # threshold
+rho = 0.02  # link density in erdos renyi network
+phi = 1.05  # efficiency of coordinated Workers
+beta = 15  # scale parameter of beta distribution
+alpha = 1  # location parameter of beta distribution
 
 
 # x_interval = np.linspace(0,1,100)
 # plt.plot(x_interval, sci.beta.cdf(x_interval, beta, alpha))
 # plt.title("beta cdf")
-#plt.show()
+# plt.show()
 
 def f_a(t, x, N, p_e, epsilon, rho, phi, beta, alpha):
-    #TODO: Marc kannst du das mal checken?!
+    # TODO: Marc kannst du das mal checken?!
     # print(x )
     if (x >= N):
         factor = 0
     else:
         factor = sci.beta.cdf(
-        (((epsilon * N) / (((N - x) * (1 - rho) ** x) +
-        ((N - x) * (1 - (1 - rho) ** x)) ** phi))),
-        a = beta, b = alpha)
+            (((epsilon * N) / (((N - x) * (1 - rho) ** x) +
+                               ((N - x) * (1 - (1 - rho) ** x)) ** phi))),
+            a=beta, b=alpha)
 
-    return(p_e * (N - 2 * x) + factor)
+    return (p_e * (N - 2 * x) + factor)
 
     # return(
     #     p_e * (N - 2 * x)  +
@@ -57,12 +57,14 @@ def f_a(t, x, N, p_e, epsilon, rho, phi, beta, alpha):
     #     a = beta, b = alpha)
     # )
 
-def f_e(x, N, rho, phi):
-    x = np.array([N if i >= N else i for i in x ])
 
-    return(
-    ((N - x) * (1 - rho) ** x + ((N - x) * (1 - (1 - rho) ** x)) ** phi) / N
+def f_e(x, N, rho, phi):
+    x = np.array([N if i >= N else i for i in x])
+
+    return (
+            ((N - x) * (1 - rho) ** x + ((N - x) * (1 - (1 - rho) ** x)) ** phi) / N
     )
+
 
 def get_timeseries(timestep, tmax, initial_value):
     r = ode(f_a)
@@ -74,12 +76,14 @@ def get_timeseries(timestep, tmax, initial_value):
     results = [initial_value]
 
     while r.t < tmax and r.successful():
-        r.integrate(r.t+timestep)
+        r.integrate(r.t + timestep)
         t.append(r.t)
         results.append(r.y)
     return t, results
+
+
 ##################################################
-p_e = 0.0 # base
+p_e = 0.0  # base
 tb, xb = get_timeseries(1, 5000, 0)
 # xb2 = np.mean(np.array(xb[0:150000]).reshape(-1,100),axis = 1)
 # tb2 = np.arange(1500)
@@ -88,10 +92,10 @@ tb, xb = get_timeseries(1, 5000, 0)
 # plt.plot(tb,f_e(np.array(xb), N, rho, phi))
 # plt.show()
 # print(f_e(np.array(xb), N, rho, phi))
-p_e = 0.02 # explore
+p_e = 0.02  # explore
 te, xe = get_timeseries(1, 5000, 0)
 
-p_e = 0.00275 # explore
+p_e = 0.00275  # explore
 ti, xi = get_timeseries(1, 5000, 0)
 
 # len(te)
@@ -99,26 +103,30 @@ ti, xi = get_timeseries(1, 5000, 0)
 # folder = input("folder name:")
 # folder = "20190419_1134"
 # folder = "20191121_1616" # pe(i)=0.003
-folder = "20191121_1732" # pe(i)=0.00275
-admin_b = np.load("../../results/model/"+folder+"/base_admin.npy")
-ecap_b = np.load("../../results/model/"+folder+"/base_ecap.npy")
+folder = "20191121_1732"  # pe(i)=0.00275
+admin_b = np.load("../../results/model/" + folder + "/base_admin.npy")
+ecap_b = np.load("../../results/model/" + folder + "/base_ecap.npy")
 
-admin_e = np.load("../../results/model/"+folder+"/expl_admin.npy")
-ecap_e = np.load("../../results/model/"+folder+"/expl_ecap.npy")
+admin_e = np.load("../../results/model/" + folder + "/expl_admin.npy")
+ecap_e = np.load("../../results/model/" + folder + "/expl_ecap.npy")
 
-admin_i = np.load("../../results/model/"+folder+"/inter_admin.npy")
-ecap_i = np.load("../../results/model/"+folder+"/inter_ecap.npy")
+admin_i = np.load("../../results/model/" + folder + "/inter_admin.npy")
+ecap_i = np.load("../../results/model/" + folder + "/inter_ecap.npy")
 
 st_b = list()
 st_i = list()
 st_e = list()
 
+# count timepoints where energy was produced for each of the 100 model runs
 for i in np.arange(100):
-    st_b.append(np.sum(ecap_b[:,i]!=0))
-    st_i.append(np.sum(ecap_i[:,i]!=0))
-    st_e.append(np.sum(ecap_e[:,i]!=0))
+    st_b.append(np.sum(ecap_b[:, i] != 0))
+    st_i.append(np.sum(ecap_i[:, i] != 0))
+    st_e.append(np.sum(ecap_e[:, i] != 0))
 
-st_e.append(5000.1)
+
+print(ecap_b)
+print(st_e)
+# st_e.append(5000.1)
 # print(st_e)
 # sns.kdeplot(st_e)
 # plt.show()
@@ -127,53 +135,51 @@ st_e.append(5000.1)
 textwidth = 12.12537
 plt.rcParams.update({'font.size': 14})
 
-cmap = cm.get_cmap("tab20",20)
-fig,(ax1,ax2,ax3) = plt.subplots(3,1,sharex = True,
-                                 figsize = (textwidth, textwidth/2))
+cmap = cm.get_cmap("tab20", 20)
+fig, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=True,
+                                    figsize=(textwidth, textwidth / 2))
 
-cv = dict(p0 = 14, p1 = 0, p2 = 2)
-
+cv = dict(p0=14, p1=0, p2=2)
 
 alpha_sim = .05
 # simulation
-ax1.plot(admin_b, color = cmap(cv["p0"]+1), alpha = alpha_sim)
-ax1.plot(admin_i, color = cmap(cv["p1"]+1), alpha = alpha_sim)
-ax1.plot(admin_e, color = cmap(cv["p2"]+1), alpha = alpha_sim)
+ax1.plot(admin_b, color=cmap(cv["p0"] + 1), alpha=alpha_sim)
+ax1.plot(admin_i, color=cmap(cv["p1"] + 1), alpha=alpha_sim)
+ax1.plot(admin_e, color=cmap(cv["p2"] + 1), alpha=alpha_sim)
 ax1.yaxis.set_label_position("right")
 ax1.set_ylabel("admin")
 
-ax2.plot(ecap_b,  color = cmap(cv["p0"]+1), alpha = alpha_sim)
-ax2.plot(ecap_i,  color = cmap(cv["p1"]+1), alpha = alpha_sim)
-ax2.plot(ecap_e,  color = cmap(cv["p2"]+1), alpha = alpha_sim)
+ax2.plot(ecap_b, color=cmap(cv["p0"] + 1), alpha=alpha_sim)
+ax2.plot(ecap_i, color=cmap(cv["p1"] + 1), alpha=alpha_sim)
+ax2.plot(ecap_e, color=cmap(cv["p2"] + 1), alpha=alpha_sim)
 ax2.yaxis.set_label_position("right")
 ax2.set_ylabel("energy $cap^{-1}$")
 
 # density plots
-sns.kdeplot(st_b, ax=ax3, color = cmap(cv["p0"]))
-sns.kdeplot(st_i, ax=ax3, color = cmap(cv["p1"]))
-sns.kdeplot(st_e, ax=ax3, color = cmap(cv["p2"]))
-ax3.set_ylim(0,0.001)
-ax3.set_xlim(-250,5250)
+ax3.hist(st_b, bins=25, color=cmap(cv["p0"]), alpha=.5, label="$p_{e}$ = 0.0")
+ax3.hist(np.array(st_e), bins=25, color=cmap(cv["p2"]), alpha=.5, label="$p_{e}$ = 0.02")
+ax3.hist(st_i, bins=25, color=cmap(cv["p1"]), alpha=.5, label="$p_{e}$ = 0.00275")
+# ax3.set_ylim(0, 0.001)
+ax3.set_xlim(-250, 5250)
 ax3.yaxis.set_label_position("right")
 ax3.set_ylabel("collapse")
 
 # analytic
-ax1.plot(tb, np.array(xb)/N, "--", color = cmap(cv["p0"]), label = "$p_{e}$ = 0.0")
-ax1.plot(ti, np.array(xi)/N, "--", color = cmap(cv["p1"]), label = "$p_{e}$ = 0.00275")
-ax1.plot(te, np.array(xe)/N, "--", color = cmap(cv["p2"]), label = "$p_{e}$ = 0.02")
+ax1.plot(tb, np.array(xb) / N, "--", color=cmap(cv["p0"]))
+ax1.plot(ti, np.array(xi) / N, "--", color=cmap(cv["p1"]))
+ax1.plot(te, np.array(xe) / N, "--", color=cmap(cv["p2"]))
 
-ax2.plot(tb, f_e(np.array(xb), N, rho, phi),"--", color = cmap(cv["p0"]))
-ax2.plot(ti, f_e(np.array(xi), N, rho, phi),"--", color = cmap(cv["p1"]))
-ax2.plot(te, f_e(np.array(xe), N, rho, phi),"--", color = cmap(cv["p2"]))
+ax2.plot(tb, f_e(np.array(xb), N, rho, phi), "--", color=cmap(cv["p0"]))
+ax2.plot(ti, f_e(np.array(xi), N, rho, phi), "--", color=cmap(cv["p1"]))
+ax2.plot(te, f_e(np.array(xe), N, rho, phi), "--", color=cmap(cv["p2"]))
 
-fig.subplots_adjust(bottom=0.08, left = 0.07, right = 0.97,top = .95)
+fig.subplots_adjust(bottom=0.08, left=0.07, right=0.97, top=.95)
 
-fig.legend(loc = "center", bbox_to_anchor = (.75,.22),ncol = 1, frameon = False)
+fig.legend(loc="center", bbox_to_anchor=(.75, .22), ncol=1, frameon=False)
 fig.text(0.5, 0.01, 'Time', ha='center', va='center')
 ax1.annotate("A", xy=(0.01, 0.83), xycoords="axes fraction")
 ax2.annotate("B", xy=(0.01, 0.83), xycoords="axes fraction")
 ax3.annotate("C", xy=(0.01, 0.83), xycoords="axes fraction")
 
-
-plt.savefig("../../results/model/"+folder+"/pub_figure3.png")
+plt.savefig("../../results/model/" + folder + "/pub_figure3.png")
 plt.show()
