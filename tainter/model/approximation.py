6372cbba0ab4df8c12373665fc82c0a5c9f64ffc
flo-schu <fluncki@protonmail.com>
2022-11-11 10:36:31 +0100

    correct mistake in approximation with alpha and beta parameters and move integration fct. to approx

--- tainter/model/approximation.py
+++ tainter/model/approximation.py
@@ -1,4 +1,6 @@
+import numpy as np
 from scipy import stats
+from scipy.integrate import odeint, trapz
 
 def f_admin(x, t, N, p_e, epsilon, rho, phi, psi, c, beta, alpha):
     """
@@ -14,8 +16,8 @@ def f_admin(x, t, N, p_e, epsilon, rho, phi, psi, c, beta, alpha):
                     ((N - x) * (1 - rho) ** x) ** psi +
                     c * ((N - x) * (1 - (1 - rho) ** x)) ** phi
                 ),
-                a=beta, 
-                b=alpha
+                a=alpha, 
+                b=beta
             )
         )
 
@@ -26,3 +28,19 @@ def f_energy(x, N, rho, phi, psi, c):
         c * ((N - x) * (1 - (1 - rho) ** x)) ** phi) / N
     )
 
+
+def get_st(t, e):
+    if all(np.round(e, 6) > 0):
+        return int(np.max(t))
+    else:
+        return int(np.where(np.round(e, 6) == 0)[0][0] + 1)
+
+
+def integrate_fa(t, params):
+    N = params["N"]
+    result = odeint(f_admin, y0=0, t=t, args=tuple(params.values())).flatten()
+    result[result > N] = N  # turn all x > N to N (fix numerical issue)
+    e = f_energy(result, N, params["rho"], params["phi"], params["psi"], params["c"])
+    st = get_st(t, e)
+    te = trapz(e[:st], t[:st])
+    return st, te, result, e
