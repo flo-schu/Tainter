07242a75bcd2aac8245f84aada4b1c690918d902
flo-schu <fluncki@protonmail.com>
2022-12-01 19:07:33 +0100

    rename name of parameter scan job

--- scripts/parameter_analysis/postprocessing.sh
+++ scripts/parameter_analysis/postprocessing.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-#SBATCH --job-name=parameter_scan                      # name of job
+#SBATCH --job-name=postprocessing_parscan              # name of job
 #SBATCH --time=0-1:00:00                               # maximum time until job is cancelled
 #SBATCH --cpus-per-task=1                              # number of nodes requested
 #SBATCH --mem-per-cpu=4G                               # memory per cpu requested
 #SBATCH --output=/work/%u/tainter/logs/%x-%A-%a.out    # output file of stdout messages
-#SBATCH --error=/work/%u/tainter/logs/%x-%A-%a.err    # output file of stderr messages
+#SBATCH --error=/work/%u/tainter/logs/%x-%A-%a.err     # output file of stderr messages
 #SBATCH --mail-type=begin                              # send mail when job begins
 #SBATCH --mail-type=end                                # send mail when job ends
 #SBATCH --mail-type=fail                               # send mail if job fails
